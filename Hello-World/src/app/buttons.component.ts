516c4e36b0981642b70d85426a36b50e190a5164
smoorthy320 <smoorthy320@gmail.com>
2019-03-31 20:40:21 +0530

    Template variables and event filtering

--- Hello-World/src/app/buttons.component.ts
+++ Hello-World/src/app/buttons.component.ts
@@ -5,8 +5,11 @@ import { Component } from "@angular/core";
         <div (click)="onDivClick()">
             <button (click)="onShow($event)" class="btn" [style.backgroundColor]="isActive ? 'green' : 'red'">Show</button>
         </div>
+        <input #email (keyup.enter)="onKeyUp(email.value)"/>
     `
     /*<button class="btn btn-primary" [class.active]="isActive" >Primary</button>*/
+    /*Event Filtering is included for enter key --<input (keyup.enter)="onKeyUp($event)"/>*/ 
+    /*Template variables by passing event and assigning it to variables --<input #email (keyup.enter)="onKeyUp(email.value)"/>*/
 })
 export class buttonsComponent{
     isActive = true;
@@ -16,5 +19,11 @@ export class buttonsComponent{
     onShow($event){
         $event.stopPropagation();
         console.log("Showing hello",$event);
+    }
+    //onKeyUp($event){
+        onKeyUp(email){
+        //if($event.keyCode === 13 ){
+            console.log("ENTER key is pressed\n",email);
+        //}
     }
 }
